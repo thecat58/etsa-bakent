5cf31727af9f3b7ca36536f4856693b348b5ab3c
thecat58 <mkike518@gmail.com>
2023-11-25 20:03:32 -0500

    se barregla

--- principal/urls.py
+++ principal/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path , include
 from rest_framework import routers
-from principal.views import *
+from .views  import *
+
 
 routers= routers.DefaultRouter()
-routers.register(r'post',PostioViewSet)
+routers.register(r'pots',postViewSet)
 
 
 urlpatterns = [
--- principal/views.py
+++ principal/views.py
@@ -1,17 +1,15 @@
-from crypt import methods
 from django.shortcuts import render
-from rest_framework.response import Response
 from rest_framework.views import APIView
 from .serializer import *
 from .models import *
 from rest_framework.decorators import action
 from django.http import Http404
+from rest_framework import viewsets
 
-
-class PostioViewSet(APIView):
-    queryset= Post=object.all()
+class postViewSet(viewsets.ModelViewSet):
+    queryset= Post.objects.all()
     serializers_class=postSerializers
 
-    @action(methods['post'],delail=True)
+    @action(methods=['post'],detail=True)
     def like_post(self, request, pk):
         post = self.get_objet()
