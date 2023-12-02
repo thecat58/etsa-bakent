1a1c8b2ed18f3555472d808edb9a963b26ac1a33
chavito017 <kb3223739115@gmail.com>
2023-12-02 09:05:00 -0500

    api

--- principal/views.py
+++ principal/views.py
@@ -21,15 +21,22 @@ class postViewSet(viewsets.ModelViewSet):
 
     
 class CitasViewSet(viewsets.ModelViewSet):
-    def get(self, request, format=None, *args, **kwargs):
+    queryset = Citas.objects.all()
+    serializer_class = CitasSerializer
+
+    # Ejemplo de un método personalizado para obtener todas las citas
+    @action(detail=False, methods=['get'])
+    def get_all_citas(self, request):
         citas = Citas.objects.all()
         serializer = CitasSerializer(citas, many=True)
         return Response(serializer.data)
 
-    def post(self, request, format=None):
+    # Método de creación de una nueva cita
+    @action(detail=False, methods=['post'])
+    def create_cita(self, request):
         serializer = CitasSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=STATUS.HTTP_201_CREATED)
-        return Response(serializer.errors, status=STATUS.HTTP_400_BAD_REQUEST)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
