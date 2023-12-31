059796951426cbda319fb156c86d2a9000b9839f
thecat58 <mkike518@gmail.com>
2023-12-31 13:42:57 -0500

    se hace troken  y toda api queda resguardara

--- principal/views.py
+++ principal/views.py
@@ -1,13 +1,22 @@
+from pstats import Stats
 from django.shortcuts import render
 from rest_framework.views import APIView
 from .serializer import *
 from rest_framework.decorators import action
-from django.http import Http404
-from rest_framework.response import Response
 from rest_framework import viewsets
-from django.contrib.auth.base_user import BaseUserManager
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authtoken.views import ObtainAuthToken
+
 
 # Create your views here.
+
+# token
+class CreateTokenView(ObtainAuthToken):
+    serializer_class = AuthTokenSerializer
+
+#
 
 
 class PostViewSet(viewsets.ModelViewSet):
@@ -18,6 +27,7 @@ class PostViewSet(viewsets.ModelViewSet):
     def like_post(self, request, pk):
         post = self.get_object()
 
+
 class TallerViewSet(viewsets.ModelViewSet):
     queryset = Taller.objects.all()
     serializer_class = TallerSerializer
@@ -27,7 +37,13 @@ class CitasViewSet(viewsets.ModelViewSet):
     queryset = Citas.objects.all()
     serializer_class = CitasSerializer
 
-    # Ejemplo de un método personalizado para obtener todas las citas
-    # @action(methods=['post'], detail=True)
-    # def like_post(self, request, pk):
-    #         post = self.get_object()
+    @api_view(['POST'])
+    def update_items(request, pk):
+        Citas = Citas.objects.get(pk=pk)
+        data = CitasSerializer(instance=Citas, data=request.data)
+
+        if data.is_valid():
+            data.save()
+            return Response(data.data)
+        else:
+            return Response(status=Stats.HTTP_404_NOT_FOUND)
