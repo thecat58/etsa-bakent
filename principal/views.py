cd4f3926dafc0bfcc91010df4e0365eb2af4e2ae
chavito07 <kb3223739115@gmail.com>
2024-02-13 14:26:24 -0500

    arreglar apis de citas quitar funciones

--- principal/views.py
+++ principal/views.py
@@ -41,9 +41,6 @@ class CreateTokenView(ObtainAuthToken):
 
 
 # este es usurio }
-
-
-
 class UsuarioViewSet(viewsets.ModelViewSet):
     serializer_class = UsuarioSerializer
     queryset = Usuario.objects.all()
@@ -72,51 +69,3 @@ class TallerViewSet(viewsets.ModelViewSet):
 class CitasViewSet(viewsets.ModelViewSet):
     queryset = Citas.objects.all()
     serializer_class = CitasSerializer
-
-    # Función personalizada para obtener todas las citas (GET)
-    @action(detail=False, methods=['get'])
-    def get_all_citas(self, request):
-     citas = Citas.objects.all()
-     serializer = CitasSerializer(citas, many=True)
-     mensaje = 'Citas obtenidas exitosamente'
-     return Response({'mensaje': mensaje, 'data': serializer.data})
-
-
-
-    # Función personalizada para crear una nueva cita (POST)
-    @action(detail=False, methods=['post'])
-    def create_cita(self, request):
-        serializer = CitasSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            # Función personalizada para crear una nueva cita (POST)
-    @action(detail=False, methods=['post'])
-    def create_cita(self, request):
-        serializer = CitasSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response({'mensaje': 'Cita creada exitosamente', 'data': serializer.data}, status=status.HTTP_201_CREATED)
-        return Response({'mensaje': 'Error al crear la cita', 'errors': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
-
-    # Función personalizada para actualizar una cita (PUT)
-    @action(detail=True, methods=['put'])
-    def update_cita(self, request, pk=None):
-        cita = self.get_object()
-        serializer = CitasSerializer(cita, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response({'mensaje': 'Cita actualizada exitosamente', 'data': serializer.data})
-        return Response({'mensaje': 'Error al actualizar la cita', 'errors': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
-
-    # Función personalizada para eliminar una cita (DELETE)
-    @action(detail=True, methods=['delete'])
-    def delete_cita(self, request, pk=None):
-        cita = self.get_object()
-        cita.delete()
-        return Response({'mensaje': 'Cita eliminada exitosamente'})
-
-    # Función que devuelve una respuesta personalizada para cualquier método
-    @action(detail=False, methods=['get', 'post', 'put', 'delete'])
-    def custom_response(self, request):
-        data = {'mensaje': 'su accion fue realizada correctamente'}
-        return Response(data)
